b9c3c586506539fef0b1b68e59b2f6b57565e57f
Sudhipth V <123481484+Sudhipth@users.noreply.github.com>
2025-01-05 20:22:10 +0530

    Time: 1 ms (98.48%), Space: 41.3 MB (85.7%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -2,10 +2,10 @@ class Solution {
     public int maxScore(String s) {
         int one=0;
         int zero=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='1')
-            one++;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                one++;
+            }
         }
         int max=Integer.MIN_VALUE;
         for(int i=0;i<s.length()-1;i++)
