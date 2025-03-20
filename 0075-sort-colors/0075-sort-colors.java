d1b18e0bfada9d4525c90d04fca4661bb357e326
Sudhipth V <123481484+Sudhipth@users.noreply.github.com>
2025-03-20 11:22:55 +0530

    Time: 0 ms (100%), Space: 42 MB (45.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,15 +1,12 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int c1=0,c2=0,c3=0;
+        int c1=0,c2=0;
      for(int i :nums){
         if(i ==0){
             c1++;
         }
         else if(i==1){
             c2++;
-        }
-        else{
-            c3++;
         }
      } 
      int j=0;
@@ -19,7 +16,7 @@ class Solution {
       for(int i =0;i<c2;i++){
         nums[j++]=1;
      }  
-      for(int i =0;i<c3;i++){
+      for(int i =c1+c2;i<nums.length;i++){
         nums[j++]=2;
      }   
     }
